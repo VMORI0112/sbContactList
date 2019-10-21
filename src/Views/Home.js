ae3cf103265a0c3be9ed5f2cf1672b4f12ade6d3
samir benzada <samirbenzada@gmail.com>
2019-10-21 16:25:35 -0400

    update

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import avatar from '../images/avatar.jpg';
+
 const Home = () => {
 
     const [data, setData] = useState([]);
@@ -21,43 +23,32 @@ const Home = () => {
                 </Link>
             </div>
 
-            {/* <ul class="list-group mt-5" id="contact-list">
-                <li class="list-group-item">
-                    <div class="col-xs-12 col-sm-3">
-                        <img src="http://api.randomuser.me/portraits/men/49.jpg" alt="Scott Stevens" class="img-fluid rounded-circle" />
-                    </div>
-                    <div class="col-xs-12 col-sm-9">
-                        <span class="name">Scott Stevens</span><br/>
-                        <span class="glyphicon glyphicon-map-marker text-muted c-info" data-toggle="tooltip" title="5842 Hillcrest Rd"></span>
-                        <span class="visible-xs"> <span class="text-muted">5842 Hillcrest Rd</span><br/></span>
-                        <span class="glyphicon glyphicon-earphone text-muted c-info" data-toggle="tooltip" title="(870) 288-4149"></span>
-                        <span class="visible-xs"> <span class="text-muted">(870) 288-4149</span><br/></span>
-                        <span class="fa fa-comments text-muted c-info" data-toggle="tooltip" title="scott.stevens@example.com"></span>
-                        <span class="visible-xs"> <span class="text-muted">scott.stevens@example.com</span><br/></span>
-                    </div>
-                    <div class="clearfix"></div>
-                </li>
-            </ul> */}
 
-
-            <ul>
+            <ul className="list-group mt-5" >
                 {!data ? ('Loading...') : data.map((item, index) => {
                     return (
-                        <li key={index}>
-                            id: {item.id}
-                            <br/>
-                            slug: {item.agenda_slug}
-                            <br/>
-                            full_name: {item.full_name}
-                            <br/>
-                            email: {item.email}
-                            <br/>
-                            phone: {item.phone}
-                            <br/>
-                            address: {item.address}
-                            <br/>
-                            create: {item.created_at}
-                        
+                        <li key={index} className="list-group-item" >
+                            <div className="row">
+                                <div className="col-2">
+                                    <img src={avatar} alt="..." className="img-fluid rounded-circle" />
+                                </div>
+                                <div className="col">
+                                    id: {item.id}
+                                    <br/>
+                                    slug: {item.agenda_slug}
+                                    <br/>
+                                    full_name: {item.full_name}
+                                    <br/>
+                                    email: {item.email}
+                                    <br/>
+                                    phone: {item.phone}
+                                    <br/>
+                                    address: {item.address}
+                                    <br/>
+                                    create: {item.created_at}
+                                </div>
+                            
+                            </div>
                         </li>
                     )
                 })}
